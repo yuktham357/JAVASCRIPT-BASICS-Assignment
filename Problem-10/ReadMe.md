88f30c203bd171ca7babdd1a1695c84ebf0ba963
Yuktha mukhi <164024317+yuktham357@users.noreply.github.com>
2025-04-30 23:15:45 +0530

    Add files via upload

---
+++ Problem-10/ReadMe.md
@@ -0,0 +1,3 @@
+**WHY THIS WORKS**
+continue skips 0, 3, and 6.
+When i === 7, break exits the loop.
---
+++ Problem-10/loops.js
@@ -0,0 +1,11 @@
+
+for (let i = 0; i < 10; i++) {
+    if (i % 3 === 0) continue;
+    if (i === 7) break;
+    console.log(i);
+  }  
+    //output
+    1
+    2
+    4
+    5
